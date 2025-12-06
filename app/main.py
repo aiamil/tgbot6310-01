483dfcb61050b69be63cb4f55fdbe0923ed336f4
aiamil <isadyrttinova@bk.ru>
2025-12-06 17:51:43 +0300

    lab6

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
+
 @bot.message_handler(content_types= ["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, f'вы написали: {message.text} ')
